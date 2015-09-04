a887093782182c8bb3f19012814afeee87dcf1b2
Carlos Eiroa <ceiroa@salesforce.com>
2015-09-04 16:52:48 -0500

    Adding another Lightning Component, to test some stuff.

--- src/aura/MyContactListApp/MyContactListApp.app
+++ src/aura/MyContactListApp/MyContactListApp.app
@@ -1,3 +1,4 @@
 <aura:application access="GLOBAL" extends="ltng:outApp">
 	<aura:dependency resource="c:MyContactList" />
+	<aura:dependency resource="c:SettingsMenu" />
 </aura:application>
---
+++ src/aura/SettingsMenu/SettingsMenuController.js
@@ -0,0 +1,10 @@
+({
+    updateTriggerLabel: function(cmp, event) {
+        var triggerCmp = cmp.find("trigger");
+        if (triggerCmp) {
+            var source = event.getSource();
+            var label = source.get("v.label");
+            triggerCmp.set("v.label", label);
+        }
+    }
+})
