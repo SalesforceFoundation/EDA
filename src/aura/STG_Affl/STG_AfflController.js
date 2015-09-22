00f144340924869f2243b2cb54e0d559d0537775
Carlos Eiroa <ceiroa@salesforce.com>
2015-09-22 12:28:00 -0500

    Retrieving more custom settings from server.

--- src/aura/STG_Affl/STG_AfflController.js
+++ src/aura/STG_Affl/STG_AfflController.js
@@ -1,6 +1,11 @@
 ({
    init : function(component, event, helper) {
 	   
-
+	   var action = component.get("c.getAfflMappings");
+	   action.setCallback(this, function(data) {
+		   console.log("Mappings: " + data.getReturnValue());
+           component.set("v.afflMappings", data.getReturnValue());
+       });
+       $A.enqueueAction(action);
    }
 })
