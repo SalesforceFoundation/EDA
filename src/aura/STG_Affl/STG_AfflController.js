9598b5ff5f6362911e88f2364cb9ac5fae754309
Carlos Eiroa <ceiroa@salesforce.com>
2015-09-24 13:46:04 -0500

    Event propagation working.

--- src/aura/STG_Affl/STG_AfflController.js
+++ src/aura/STG_Affl/STG_AfflController.js
@@ -1,10 +1,16 @@
 ({
    init : function(component, event, helper) {
+	   component.set("v.isView", true);
 	   
 	   var action = component.get("c.getAfflMappings");
 	   action.setCallback(this, function(data) {
            component.set("v.afflMappings", data.getReturnValue());
        });
        $A.enqueueAction(action);
+   },
+   
+   toggleIsView : function(component, event, helper) {
+	   console.log('reading edit/save event');
+	   component.set("v.isView", event.getParam("isView"));
    }
 })
--- src/aura/STG_Header/STG_HeaderController.js
+++ src/aura/STG_Header/STG_HeaderController.js
@@ -1,15 +1,21 @@
 ({
 	init : function(component, event, helper) {
-		component.set("v.edit", true);
+		component.set("v.isView", true);
 	},
 	
 	edit : function(component, event, helper) {
-		console.log("Edit clicked");
-		component.set("v.edit", false);
+		component.set("v.isView", false);
+		console.log('firing edit');
+		var editEvent = $A.get("e.c:editEvent");
+		console.log(JSON.parse(JSON.stringify(editEvent)));
+		editEvent.setParams({"isView": false});
+		console.log(JSON.parse(JSON.stringify(editEvent)));
+		editEvent.fire();
 	},
 	
 	save : function(component, event, helper) {
-		console.log("Save clicked");
-		component.set("v.edit", true);
+		component.set("v.isView", true);
+		console.log('firing save');
+		$A.get("e.c:editEvent").setParams({"isView": true}).fire();
 	}
 })
