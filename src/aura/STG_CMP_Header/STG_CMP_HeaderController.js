64aa28a5e68cc5dfd550b481c749a82c5edd6dfd
Carlos Eiroa <ceiroa@salesforce.com>
2015-09-25 10:37:40 -0500

    Adding Save event. Trying to save hierarchy settings (unsuccessfully, but at least the event gets handled).

--- src/aura/STG_CMP_Header/STG_CMP_HeaderController.js
+++ src/aura/STG_CMP_Header/STG_CMP_HeaderController.js
@@ -10,7 +10,7 @@
 	
 	save : function(component, event, helper) {
 		component.set("v.isView", true);
-		$A.get("e.c:STG_EVT_Edit").setParams({"isView": true}).fire();
+		$A.get("e.c:STG_EVT_Save").setParams({"save": true}).fire();
 	},
 	
 	cancel : function(component, event, helper) {
--- src/aura/STG_CMP_Tabs/STG_CMP_TabsController.js
+++ src/aura/STG_CMP_Tabs/STG_CMP_TabsController.js
@@ -7,11 +7,32 @@
 		$A.util.addClass(component.find("addrTabContent"), "slds-hide");
 		$A.util.addClass(component.find("systemTabContent"), "slds-hide");
 		
+		//Retrieving hierarchy settings.
 		var action = component.get("c.getSettings");
 	    action.setCallback(this, function(data) {
 	        component.set("v.hierarchySettings", data.getReturnValue());
 	    });
 	    $A.enqueueAction(action);
+	},
+	
+	saveSettings : function(component, event, helper) {
+		var saveAction = component.get("c.saveHierarchySettings");
+		saveAction.setParams({settings : component.get("v.hierarchySettings")});
+		saveAction.setCallback(this, function(response) {
+			if(response.getState() === "SUCCESS") {
+				component.set("v.sucessMessage", "Saved");
+			} else if(response.getState() === "ERROR") {
+				var errors = response.getError();
+				if (errors) {
+					if (errors[0] && errors[0].message) {
+						$A.error("Error message: " + errors[0].message);
+					}
+				} else {
+					$A.error("Unknown error");
+				}
+			}
+		});
+		$A.enqueueAction(saveAction);
 	},
 
 	afflLinkClicked : function(component, event, helper) {
