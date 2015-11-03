12523cf41255dc322618b4420d9f910625940267
Carlos Eiroa <ceiroa@salesforce.com>
2015-11-03 11:56:27 -0600

    Cleaning things up. Adding prefix when necessary to view settings. Started removing it for saving settings.

--- src/aura/STG_CMP_Affl/STG_CMP_AfflHelper.js
+++ src/aura/STG_CMP_Affl/STG_CMP_AfflHelper.js
@@ -6,8 +6,27 @@
 
 	loadAfflMappings : function(component) {
 		var action = component.get("c.getAfflMappings");
-		action.setCallback(this, function(response) {
-			component.set("v.afflMappings", response.getReturnValue());
+		action.setCallback(this, function(response) {		
+			if(response.getState() === "SUCCESS") {
+				if(namespace_prefix && namespace_prefix.length > 0) {
+		    		var settings = response.getReturnValue();
+		    		var settings_no_prefix = {};
+		    		//Remove package prefix from each custom field
+		    		for(var key in settings) {
+		    			if(key.endsWith('__c') && namespace_prefix && namespace_prefix.length > 0) {
+		    				var key_no_prefix = key.replace(namespace_prefix, '');
+			    			settings_no_prefix[key_no_prefix] = settings[key];
+		    			} else {
+		    				settings_no_prefix[key] = settings[key];
+		    			}
+		    		}
+		    		component.set("v.afflMappings", settings_no_prefix);
+				} else {
+					component.set("v.afflMappings", response.getReturnValue());
+				}
+	    	} else if(response.getState() === "ERROR") {
+	    		this.displayError(response);
+	    	}	
 		});
 		$A.enqueueAction(action);
 	},
@@ -18,17 +37,32 @@
 	
 	saveAfflSettings : function(component) {
 		var saveAction = component.get("c.saveAfflMappings");
-		saveAction.setParams({"afflMappings" : component.get("v.afflMappings")});
+		var settings = component.get("v.afflMappings");
+		var settings_w_prefix = {};
+		//Add package prefix to each custom field
+		for(var key in settings) {
+			if(key.endsWith('__c')) {
+				var key_w_prefix = namespace_prefix + key;
+    			settings_w_prefix[key_w_prefix] = settings[key];
+			} else {
+				settings_w_prefix[key] = settings[key];
+			}
+		}
+		saveAction.setParams({"afflMappings" : settings_w_prefix});
 		saveAction.setCallback(this, function(response) {
 			if(response.getState() === "ERROR") {
-				var errors = response.getError();
-				if (errors && errors[0] && errors[0].message) {
-					$A.error("Error message: " + errors[0].message);
-				} else {
-					$A.error("Unknown error");
-				}
+				this.displayError(response);
 			}
 		});
 		$A.enqueueAction(saveAction);
+	},
+	
+	displayError : function(response) {
+		var errors = response.getError();
+		if (errors && errors[0] && errors[0].message) {
+			$A.error("Error message: " + errors[0].message);
+		} else {
+			$A.error("Unknown error");
+		}
 	}
 })
