1e39b1371ee57d6316b8b499be72451f3178cbf7
Carlos Eiroa <ceiroa@salesforce.com>
2015-11-04 16:48:44 -0600

    This should actually work with hierarchy settings (the other way was for list settings).

--- src/aura/STG_CMP_Base/STG_CMP_BaseHelper.js
+++ src/aura/STG_CMP_Base/STG_CMP_BaseHelper.js
@@ -42,20 +42,15 @@
 	
 	addPrefixHierarchySettings : function(settings, namespacePrefix) {
 		if(namespacePrefix && namespacePrefix.length > 0) {
-			//We need to end up with an array of objects, because the settings param is an array of objects
-    		var settings_w_prefix = []; //create array
+    		var settings_w_prefix = {};
     		//Remove package prefix from each custom field
-    		for(var key in settings) { //Iterate over each row - key is just a numeric index here
-    			var obj = {}; //create object
-    			for(var key2 in settings[key]) { //Iterate over each field in each row - key2 is the actual field name
-    				if(key2.endsWith('__c')) { 
-	    				var key2_w_prefix = namespacePrefix + key;
-	    				obj[key2_w_prefix] = settings[key][key2];
-	    			} else {
-	    				obj[key2] = settings[key][key2];
-	    			}
+    		for(var key in settings) { //Iterate over each row
+    			if(key.endsWith('__c')) {
+    				var key_w_prefix = namespacePrefix + key;
+	    			settings_w_prefix[key_w_prefix] = settings[key];
+    			} else {
+    				settings_w_prefix[key] = settings[key];
     			}
-    			settings_w_prefix.push(obj);
     		}
     		return settings_w_prefix;
 		} else {
