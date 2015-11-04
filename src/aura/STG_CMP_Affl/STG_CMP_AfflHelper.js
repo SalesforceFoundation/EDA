f7c7b194805609021c74110f382bf4020c2f8fff
Carlos Eiroa <ceiroa@salesforce.com>
2015-11-04 14:03:38 -0600

    Attempting lightning components code reuse through inheritance.

--- src/aura/STG_CMP_Affl/STG_CMP_AfflHelper.js
+++ src/aura/STG_CMP_Affl/STG_CMP_AfflHelper.js
@@ -6,38 +6,16 @@
 
 	loadAfflMappings : function(component) {
 		var action = component.get("c.getAfflMappings");
+		var namespacePrefix = component.get("v.namespacePrefix");
 		action.setCallback(this, function(response) {		
 			if(response.getState() === "SUCCESS") {
 				var settings = response.getReturnValue();
-				component.set("v.afflMappings", this.getProcessedSettings(settings));
+				component.set("v.afflMappings", this.getProcessedListSettings(settings, namespacePrefix));
 	    	} else if(response.getState() === "ERROR") {
 	    		this.displayError(response);
 	    	}	
 		});
 		$A.enqueueAction(action);
-	},
-	
-	getProcessedSettings : function(settings) {
-		if(namespacePrefix && namespacePrefix.length > 0) {
-			//We need to end up with an array of objects, because the settings param is an array of objects
-    		var settings_no_prefix = []; //create array
-    		//Remove package prefix from each custom field
-    		for(var key in settings) { //Iterate over each row - key is just a numeric index here
-    			var obj = {}; //create object
-    			for(var key2 in settings[key]) { //Iterate over each field in each row - key2 is the actual field name
-    				if(key2.endsWith('__c')) { 
-	    				var key2_no_prefix = key2.replace(namespacePrefix, '');
-	    				obj[key2_no_prefix] = settings[key][key2];
-	    			} else {
-	    				obj[key2] = settings[key][key2];
-	    			}
-    			}
-    			settings_no_prefix.push(obj);
-    		}
-    		return settings_no_prefix;
-		} else {
-			return settings;
-		}
 	},
 	
 	resetSettings : function(component) {
@@ -46,6 +24,7 @@
 	
 	saveAfflSettings : function(component) {
 		var saveAction = component.get("c.saveAfflMappings");
+		var namespacePrefix = component.get("v.namespacePrefix");
 		var settings = component.get("v.afflMappings");
 		if(namespacePrefix && namespacePrefix.length > 0) {
 			var settings_w_prefix = {};
@@ -68,14 +47,5 @@
 			}
 		});
 		$A.enqueueAction(saveAction);
-	},
-	
-	displayError : function(response) {
-		var errors = response.getError();
-		if (errors && errors[0].pageErrors[0] && errors[0].pageErrors[0].message) {
-			$A.error("Error message: " + errors[0].message);
-		} else {
-			$A.error("Unknown error");
-		}
 	}
 })
