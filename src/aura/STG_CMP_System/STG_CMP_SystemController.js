feddd8a770ab98d0abd72e2fa3c8177b6b9b3def
Carlos Eiroa <ceiroa@salesforce.com>
2016-03-29 13:46:39 -0500

    Properly displaying stored acc record type when changing to edit view, and properly saving.

--- src/aura/STG_CMP_System/STG_CMP_SystemController.js
+++ src/aura/STG_CMP_System/STG_CMP_SystemController.js
@@ -5,5 +5,9 @@
 	
 	toggleIsView : function(component, event, helper) {
 		component.set("v.isView", event.getParam("isView"));
+	},
+	
+	saveAccRecType : function(component, event, helper) {
+		helper.saveAccRecType(component, event);
 	}
 })
