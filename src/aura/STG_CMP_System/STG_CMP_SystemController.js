1e4047cd268fe26d07c6bfeadf86d51c95b683ad
Carlos Eiroa <ceiroa@salesforce.com>
2016-08-15 14:22:53 -0500

    Removing unused code.

--- src/aura/STG_CMP_System/STG_CMP_SystemController.js
+++ src/aura/STG_CMP_System/STG_CMP_SystemController.js
@@ -1,9 +1,5 @@
 ({
 	toggleIsView : function(component, event) {
 		component.set("v.isView", event.getParam("isView"));
-	},
-
-	saveAccRecType : function(component, event, helper) {
-		helper.saveAccRecType(component, event);
 	}
 })
