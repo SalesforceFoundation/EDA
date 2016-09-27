5c8d4595e825a631aad7dbcab87306083ee69b1b
Paul Malenke <pmalenke@salesforce.com>
2016-09-27 15:17:43 -0500

    Fixes #280. The CMP_RecTypesHelper should check that there are record types available for save. Also moved selected logic inside check so settings are not overwritten if they exist.

--- src/aura/CMP_RecTypes/CMP_RecTypesHelper.js
+++ src/aura/CMP_RecTypes/CMP_RecTypesHelper.js
@@ -1,21 +1,21 @@
 ({
-	saveSettings : function(component) {
-		var selectedTypes = "";
-		var typeCheckboxes = component.find("checkboxIds");
-
-		for(var i = 0; i <= typeCheckboxes.length; i += 1) {
-			var checkbox = typeCheckboxes[i];
-			if(typeof checkbox !== 'undefined') {
-				//var checkboxLabel = checkbox.get("v.label");
-				var checkboxVal = checkbox.get("v.value");
-				if(checkboxVal === true) {
-					var checkboxText = checkbox.get("v.text");
-					selectedTypes += checkboxText + '; ';
-				}
-			}
-		}
-		var setting = component.get("v.setting");
-		setting = selectedTypes;
-		component.set("v.setting", setting);
-	}
+  saveSettings : function(component) {
+    var typeCheckboxes = component.find("checkboxIds");
+    if (typeCheckboxes != null) {
+      var selectedTypes = "";
+      for(var i = 0; i <= typeCheckboxes.length; i += 1) {
+        var checkbox = typeCheckboxes[i];
+        if(typeof checkbox !== 'undefined') {
+          var checkboxVal = checkbox.get("v.value");
+          if(checkboxVal === true) {
+            var checkboxText = checkbox.get("v.text");
+            selectedTypes += checkboxText + '; ';
+          }
+        }
+      }
+      var setting = component.get("v.setting");
+      setting = selectedTypes;
+      component.set("v.setting", setting);
+    }
+  }
 })
