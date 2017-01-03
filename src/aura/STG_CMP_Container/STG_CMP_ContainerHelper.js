fcaa2160b99a7b969dc9e513638639d8e2b95e33
David Ray <david@raycreative.com>
2017-01-03 09:32:01 -0800

    Removing console.log()

--- src/aura/STG_CMP_Container/STG_CMP_ContainerHelper.js
+++ src/aura/STG_CMP_Container/STG_CMP_ContainerHelper.js
@@ -1,7 +1,5 @@
 ({
 	showToast: function(component, event, helper, message) {
-		console.log("TOASTEr!");
-
 		$A.util.addClass(component.find("notificationToast"), "slds-show");
 		$A.util.removeClass(component.find("notificationToast"), "slds-hide");
 	},
