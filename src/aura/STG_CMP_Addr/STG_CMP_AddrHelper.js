f266ea0e66103a442b7f17165ce09ef8ff8f3935
David Ray <dray@salesforce.com>
2017-04-07 23:36:06 -0700

    Updated batch for contact processing and added batch button to settings

--- src/aura/STG_CMP_Addr/STG_CMP_AddrHelper.js
+++ src/aura/STG_CMP_Addr/STG_CMP_AddrHelper.js
@@ -12,5 +12,19 @@
 		    }
 		});
 		$A.enqueueAction(runBatchAction);
+	},
+	runCleanUp : function (component, event, helper) {
+		var runBatchAction = component.get("c.executePreferredEmailCleanUpBatch");
+		runBatchAction.setCallback(this, function(response) {
+	        if(response.getState() === "SUCCESS") {
+
+			    var statusMsg = component.find("cleanUpMsg");
+			    $A.util.toggleClass(statusMsg, "slds-hide");
+
+	        } else if(response.getState() === "ERROR") {
+	        	this.displayError(response);
+		    }
+		});
+		$A.enqueueAction(runBatchAction);
 	}
 })
