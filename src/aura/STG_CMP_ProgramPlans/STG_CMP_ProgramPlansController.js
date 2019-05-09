594eacc0618c6b128a780ab3927e255429972a33
Xinnan Li <xinnan.li@salesforce.com>
2019-05-09 16:30:25 -0400

    Wip

---
+++ src/aura/STG_CMP_ProgramPlans/STG_CMP_ProgramPlansController.js
@@ -0,0 +1,10 @@
+({
+    init : function(component, event, helper) {
+        helper.init(component);
+    },
+
+    toggleIsView: function (component, event, helper) {
+        component.set("v.isView", event.getParam("isView"));
+    }
+
+})
