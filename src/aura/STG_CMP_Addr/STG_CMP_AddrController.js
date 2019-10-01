6a5d7df7dd4da0712d3dd1734ae1226ce501df30
skosgi1 <samsheeth.kosgi@gmail.com>
2019-10-01 16:54:29 -0700

    changes based on feedback

--- src/aura/STG_CMP_Addr/STG_CMP_AddrController.js
+++ src/aura/STG_CMP_Addr/STG_CMP_AddrController.js
@@ -2,8 +2,6 @@
     toggleIsView : function(component, event, helper) {
         component.set("v.isView", event.getParam("isView"));
         var enablePrefPhone = component.find("enablePrefPhoneEdit").get("v.value");
-
-        helper.toggleDisablePhoneEnforcementCheckbox(component, enablePrefPhone);
     },
     runBackfill : function (component, event, helper) {
         helper.runBackfill(component);
