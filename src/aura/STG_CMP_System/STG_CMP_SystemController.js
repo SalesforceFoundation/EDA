fd6f000a9b35052da113abc13072585a0eb44c4f
Kimberly Dy <kdy@salesforce.com>
2019-11-25 11:34:57 -0500

    Added new batch for administrative and updated lightning cmp to call the new batch class.

--- src/aura/STG_CMP_System/STG_CMP_SystemController.js
+++ src/aura/STG_CMP_System/STG_CMP_SystemController.js
@@ -6,13 +6,13 @@
 		if( !event.getParam("isView") ) {
 			var notifyTo = component.get("v.hierarchySettings.Error_Notifications_To__c"),
 				type = "All Sys Admins";
-			
+
 			if (notifyTo.startsWith("005")) { // User
 				type = "User";
 			} else if ( notifyTo.startsWith("0F9") ) { // Chatter Group
 				type = "Chatter Group";
 			}
-			
+
 			component.set("v.errorNotificationType", type);
 
 			// process dropdown to display select if necessary
@@ -23,27 +23,28 @@
 	errorToSelect : function(component, event, helper) {
 		helper.errorToSelect(component);
 	},
-    handleAutocomplete: function(component, event) {
+    handleAutocomplete : function(component, event) {
         var selOpt = event.getParam('value');
         component.set("v.hierarchySettings.Error_Notifications_To__c", selOpt.value);
     },
-    
-    settsLinkClicked: function(component, event, helper) {
-        console.log('InsideJS settsclicked');
+
+    settsLinkClicked : function(component, event, helper) {
         helper.settsLinkClicked(component, event, helper);
-    }, 
-    bulkProcessLinkClicked: function(component, helper) {
-        console.log('InsideJS bulkProcessLinkClicked');
+    },
+    bulkProcessLinkClicked : function(component, helper) {
         helper.bulkProcessLinkClicked(component, helper);
     },
-    
-    handleRefreshHouseholdAccount: function(component, event, helper) {
-        console.log('InsideJS handleRefresh');
-        helper.handleRefreshHouseholdAccount(component, event, helper); 
-    }, 
-    
-    handleRefreshAdminAccount: function(component, event, helper) {
-        console.log('InsideJS handleAcctRefresh');
-        helper.handleRefreshAdminAccount(component, event, helper); 
-    }
+
+    handleRefreshHouseholdAccount : function(component, event, helper) {
+        helper.handleRefreshHouseholdAccount(component, event, helper);
+    },
+
+    handleRefreshAdminAccount : function(component, event, helper) {
+        helper.handleRefreshAdminAccount(component, event, helper);
+    },
+
+    closeToast : function(component, event, helper) {
+    helper.closeToast(component);
+
+  },
 })
--- src/aura/STG_CMP_System/STG_CMP_SystemHelper.js
+++ src/aura/STG_CMP_System/STG_CMP_SystemHelper.js
@@ -13,7 +13,7 @@
 			$A.util.addClass(errNoticeChatter, "slds-hide");
 
 		} else if (notifyType==="Chatter Group") {
-			
+
 			$A.util.removeClass(errNoticeChatter, "slds-hide");
 			$A.util.addClass(errNoticeChatter, "slds-show");
 			// Hide other field if shown
@@ -30,66 +30,48 @@
 			$A.util.removeClass(errNoticeUserId, "slds-show");
 			$A.util.addClass(errNoticeUserId, "slds-hide");
 		}
-	}, 
-    
-    /*settsLinkClicked: function(component, event, helper) {
-        console.log('InsideHP settsLinkClicked'); 
-        var buttonClick = component.find("settsTabContent"); 
-        var settsTabContent = component.find("settsTabContent");
-        var bulkProcessTabContent = component.find("bulkProcessTabContent"); 
-        console.log(buttonClick); 
-        console.log(settsTabContent); 
-        
-        if (buttonClick === "settsTabContent") {
-            console.log('settsIf -->' + buttonClick); 
-            $A.util.addClass(component.find("settsTab"), "slds-active");
-        	$A.util.removeClass(component.find("bulkTab"), "slds-active");
-            //$A.util.removeClass(settsTabContent, "slds-hide");
-            //$A.util.addClass(settsTabContent, "slds-show");
-            //$A.util.removeClass(bulkProcessTabContent, "slds-hide");
-        }
-         console.log('InsideHP settsLinkClicked AFTER'); 
-        //this.hideTabContent(component,"bulkProcessingTabContent");
-    },
-    
-    bulkProcessLinkClicked: function(component, helper) {
-        console.log('InsideHP bulkProcessLinkClicked'); 
-        $A.util.addClass(component.find("bulkTab"), "slds-active");
-        $A.util.removeClass(component.find("settsTab"), "slds-active");
-        
-        var bulkProcessTabContent = component.find("bulkProcessTabContent");
-        $A.util.removeClass(bulkProcessTabContent, "slds-hide");
-        $A.util.addClass(bulkProcessTabContent, "slds-show");
+	},
 
-        this.hideTabContent(component,"settsTabContent");
-        
-    }, */
-    
     handleRefreshHouseholdAccount: function(component, event, helper) {
-        console.log('InsideHelperforHandleRefresh'); 
-    	var action = component.get("c.executeRefreshHouseholdAccountBatch"); 
-        console.log('InsideHelperforHandleRefresh AfterAction'); 
+    	var action = component.get("c.executeRefreshHouseholdAccountBatch");
         action.setCallback(this, function(response) {
-                            var state = response.getState(); 
+                            var state = response.getState();
         if (state === "SUCCESS") {
-            console.log('InsideHelperforHandleRefresh Success'); 
-            
-            var toast = component.find("successToast"); 
+            var toast = component.find("successToast");
         	$A.util.removeClass(toast,'slds-hide');
-            $A.util.addClass(toast, 'slds-show'); 
-            console.log('InsideHelperforHandleRefresh Success2'); 
+            $A.util.addClass(toast, 'slds-show');
         } else if (state ==="INCOMPLETE") {
-            console.log('InsideHelperforHandleRefresh Incomplete'); 
+            console.log('InsideHelperforHandleRefresh Incomplete');
         } else if (state ==="ERROR") {
-            console.log('InsideHelperforHandleRefresh ERROR'); 
+            console.log('InsideHelperforHandleRefresh ERROR');
         }
-        }); 
-        
-        $A.enqueueAction(action); 
+        });
+
+        $A.enqueueAction(action);
     },
-    
+
     handleRefreshAdminAccount: function(component, event, helper) {
-    	    
-    }
+    	var action = component.get("c.executeRefreshAdminAccountBatch");
+        action.setCallback(this, function(response) {
+                            var state = response.getState();
+        if (state === "SUCCESS") {
+            var toast = component.find("successToast");
+        	$A.util.removeClass(toast,'slds-hide');
+            $A.util.addClass(toast, 'slds-show');
+        } else if (state ==="INCOMPLETE") {
+            console.log('InsideHelperforHandleRefresh Incomplete');
+        } else if (state ==="ERROR") {
+            console.log('InsideHelperforHandleRefresh ERROR');
+        }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    closeToast: function(component) {
+        var tst = component.find("successToast");
+        $A.util.removeClass(tst, "slds-show");
+        $A.util.addClass(tst, "slds-hide");
+    },
 
 })
