6c5439396fd8a2d6770516b28593ede6f7e0bec1
Ryan Headley <lifewithryan@gmail.com>
2020-02-05 09:50:05 -0600

    just fixing some tabs vs. spaces stuff

--- src/aura/STG_CMP_System/STG_CMP_SystemHelper.js
+++ src/aura/STG_CMP_System/STG_CMP_SystemHelper.js
@@ -1,59 +1,61 @@
 ({
-	errorToSelect : function(component) {
-		var errNoticeUserId = component.find("errNoticeUserId");
-		var errNoticeChatter = component.find("errNoticeChatter");
-		var	notifyType = component.get("v.errorNotificationType");
+    errorToSelect : function(component) {
+        var errNoticeUserId = component.find("errNoticeUserId");
+        var errNoticeChatter = component.find("errNoticeChatter");
+        var	notifyType = component.get("v.errorNotificationType");
 
-		if (notifyType==="User") {
+        if (notifyType==="User") {
 
-			$A.util.removeClass(errNoticeUserId, "slds-hide");
-			$A.util.addClass(errNoticeUserId, "slds-show");
-			// Hide other field if shown
-			$A.util.removeClass(errNoticeChatter, "slds-show");
-			$A.util.addClass(errNoticeChatter, "slds-hide");
+            $A.util.removeClass(errNoticeUserId, "slds-hide");
+            $A.util.addClass(errNoticeUserId, "slds-show");
+            // Hide other field if shown
+            $A.util.removeClass(errNoticeChatter, "slds-show");
+            $A.util.addClass(errNoticeChatter, "slds-hide");
 
-		} else if (notifyType==="Chatter Group") {
-			
-			$A.util.removeClass(errNoticeChatter, "slds-hide");
-			$A.util.addClass(errNoticeChatter, "slds-show");
-			// Hide other field if shown
-			$A.util.removeClass(errNoticeUserId, "slds-show");
-			$A.util.addClass(errNoticeUserId, "slds-hide");
+        } else if (notifyType==="Chatter Group") {
+            
+            $A.util.removeClass(errNoticeChatter, "slds-hide");
+            $A.util.addClass(errNoticeChatter, "slds-show");
+            // Hide other field if shown
+            $A.util.removeClass(errNoticeUserId, "slds-show");
+            $A.util.addClass(errNoticeUserId, "slds-hide");
 
-		} else {
-			// User selected "All Sys Admins"
-			component.set("v.hierarchySettings.Error_Notifications_To__c","All Sys Admins");
+        } else {
+            // User selected "All Sys Admins"
+            component.set("v.hierarchySettings.Error_Notifications_To__c","All Sys Admins");
 
-			// Hide other fields
-			$A.util.addClass(errNoticeChatter, "slds-hide");
-			$A.util.removeClass(errNoticeChatter, "slds-show");
-			$A.util.removeClass(errNoticeUserId, "slds-show");
-			$A.util.addClass(errNoticeUserId, "slds-hide");
-		}
-	}, 
+            // Hide other fields
+            $A.util.addClass(errNoticeChatter, "slds-hide");
+            $A.util.removeClass(errNoticeChatter, "slds-show");
+            $A.util.removeClass(errNoticeUserId, "slds-show");
+            $A.util.addClass(errNoticeUserId, "slds-hide");
+        }
+    }, 
     
     handleRefreshHouseholdAccount: function(component, event, helper) {
-    	var action = component.get("c.executeRefreshHouseholdAccountBatch"); 
+        var action = component.get("c.executeRefreshHouseholdAccountBatch"); 
         action.setCallback(this, function(response) {
-                            var state = response.getState(); 
-        if (state === "SUCCESS") {            
-            var toast = component.find("hhSuccessToast"); 
-        	$A.util.removeClass(toast,'slds-hide');
-            $A.util.addClass(toast, 'slds-show'); 
-        }}); 
+            var state = response.getState(); 
+            if (state === "SUCCESS") {            
+                var toast = component.find("hhSuccessToast"); 
+                $A.util.removeClass(toast,'slds-hide');
+                $A.util.addClass(toast, 'slds-show'); 
+            }
+        }); 
         
         $A.enqueueAction(action); 
     },
     
     handleRefreshAdminAccount: function(component, event, helper) {
-    	var action = component.get("c.executeRefreshAdminAccountBatch"); 
+        var action = component.get("c.executeRefreshAdminAccountBatch"); 
         action.setCallback(this, function(response) {
-                            var state = response.getState(); 
-        if (state === "SUCCESS") {            
-            var toast = component.find("adminSuccessToast"); 
-        	$A.util.removeClass(toast,'slds-hide');
-            $A.util.addClass(toast, 'slds-show'); 
-        }}); 
+            var state = response.getState(); 
+            if (state === "SUCCESS") {            
+                var toast = component.find("adminSuccessToast"); 
+                $A.util.removeClass(toast,'slds-hide');
+                $A.util.addClass(toast, 'slds-show'); 
+            }
+        }); 
         
         $A.enqueueAction(action);	    
     },
