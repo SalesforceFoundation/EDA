f1f111f6142b70d5855043fb4110df7ff4962afb
Rakesh Jeyaraman <rjeyaraman@salesforce.com>
2020-08-21 14:03:22 -0400

    updated locators file

--- robot/EDA/resources/locators_49.py
+++ robot/EDA/resources/locators_49.py
@@ -117,7 +117,6 @@ eda_lex_locators = {
         "backfill_button_status": "//span[text()='{}']/parent::button",
         "backfill_checkbox_status": "//input[contains(@class, 'backfill')]/following-sibling::span[contains(@class, 'checkbox')]",
     },
-<<<<<<< HEAD
     "eda_settings_program_plans": {
         "checkbox_read": "//span[text()='{}']/../following-sibling::div/descendant::img",
         "checkbox_edit": "(//span[text()='{}']/../following-sibling::div/descendant::span)[1]",
@@ -126,12 +125,9 @@ eda_lex_locators = {
     "eda_settings_courses": {
         "text_message": "//span[text()='{}']",
     },
-||||||| 5612c7ae
-=======
     "eda_settings_courses": {
         "text_message": "//span[text()='{}']",
     },
->>>>>>> 068e5b61bfa96ed14ee91e3bde6c0b32425bdac2
     "eda_settings_system": {
         "default_checkbox": "//span[text()='{}']/../following-sibling::div[1]/descendant::img",
         "default_dropdown_value": "//span[text()='{}']/../following-sibling::div[1]/descendant::span[text()='{}']",
