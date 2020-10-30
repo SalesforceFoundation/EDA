ff17c5cb8f8bbe52bf61ffe33bb7d58e27de0cb7
m-kinkade <merle.kinkade@salesforce.com>
2020-10-30 13:36:53 -0700

    initial commit

--- src/lwc/objectSelector/objectSelector.js
+++ src/lwc/objectSelector/objectSelector.js
@@ -1,6 +1,7 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
 
-import getFieldSelectOptions from '@salesforce/apex/CTRL_FieldSelectOption.getFieldSelectOptions';
+//import getFieldSelectOptions from '@salesforce/apex/CTRL_FieldSelectOption.getFieldSelectOptions';
+import getPickListOptions from '@salesforce/apex/CTRL_FieldSelectOption.getPickListOptions';
 
 import errorToast from 'c/toastHandler';
 
@@ -9,12 +10,28 @@ import objectSelectComboboxLabel from '@salesforce/label/c.objectSelectComboboxL
 import objectSelectComboboxPlaceholder from '@salesforce/label/c.objectSelectComboboxPlaceholder';
 
 export default class ObjectSelector extends LightningElement {
+    // parameters will be passed in from the parent component - hardcoding for testing only
+    @api objectAPIName = 'Contact';
+    @api fieldAPIName = 'Country_of_Origin__c';
+
     value = 'settingsValue';
 
+    // load picklist field options based on object and field API names
+    @wire(getPickListOptions, {
+        objectAPIName: '$objectAPIName', 
+        fieldAPIName: '$fieldAPIName',
+    }) wiredPicklistViewModel;
+        // ({ error, data}){
+        //     if (data){
+        //         console.log('picklist values successfully retrieved! Data retrieved: ' + JSON.stringify(data));
+        //         //TODO: populate picklistItemsList with values retrieved from data
+        //     } else if (error){
+        //         console.log('error retrieving picklist values');
+        //     }
+        // };
+
     //This can use an API to pull the information from the parent component
-    @wire(getFieldSelectOptions, {
-        apiNameList: ['Contact','Account'],
-    }) viewModel;
+    //@wire(getFieldSelectOptions, {apiNameList: ['Contact']}) viewModel;
 
     //This label reference object lets us embed labels
     labelReference = {
@@ -24,11 +41,24 @@ export default class ObjectSelector extends LightningElement {
     };
 
     get comboboxOptions() {
-        if(!this.viewModel.data) {
+        console.log('Getting picklist for object....... ');
+
+        if(!this.wiredPicklistViewModel.data) {
             return null;
         }
 
-        return this.viewModel.data.fieldSelectOptions;
+        return this.wiredPicklistViewModel.data.fieldSelectOptions;
+
+        // let fieldSelectModels = data[0].fieldSelectOptions;
+        // for (i=0; i<fieldSelectModels.length; i++){
+        //     console.log('picklist field name: ' + fieldSelectModels[i].value);
+        // }
+
+        // if (!this.wiredPicklistViewModel.data){
+        //     console.log('wiredPicklistViewModel null');
+        // }
+
+        // return this.wiredPicklistViewModel.data.fieldSelectOptions;
     }
 
     //This would kick off a new event to a parent component.
