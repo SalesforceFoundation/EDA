a58ec118cfdf35d0edc69eb2eef83afdbdf2fc4d
m-kinkade <merle.kinkade@salesforce.com>
2020-11-12 17:01:36 -0800

    added methods to filter by lookup SObjecttype

---
+++ src/lwc/objectSelectorLookupFilter/objectSelectorLookupFilter.js
@@ -0,0 +1,8 @@
+import { LightningElement, wire, api } from 'lwc';
+
+import getAccountLookupFields from '@salesforce/apex/CTRL_FilteredLookupComboBox.getAccountLookupFields';
+
+
+export default class ObjectLookupSelector extends LightningElement {
+
+}
