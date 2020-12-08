6a03076797620c5bfa5556f095e3334f7d72da26
m-kinkade <merle.kinkade@salesforce.com>
2020-12-08 11:25:56 -0800

    added custom labels

--- src/lwc/healthCheck/healthCheck.js
+++ src/lwc/healthCheck/healthCheck.js
@@ -5,14 +5,16 @@ export default class HealthCheck extends LightningElement {
     @track totalChecks = 0;
     @track passedChecks = 0;
 
-    labelReference = {
-        edaSetup: 'EDA Setup',
-        healthCheckTitle: 'Health Check',
-        runHealthCheckButton: 'Run Health Check',
-        passedChecks: 'Checks Passed: {0}/{1}',
-        statusColumnHeader: 'Status',
-        settingColumnHeader: 'Setting',
-        descriptionColumnHeader: 'Description',
-        recommendedFixColumnHeader: 'Recommended Fix',
-    }
+    lastRunDate = new Date().toLocaleDateString("en-US"); // hardcoded for now
+
+    // labelReference = {
+    //     edaSetup: 'EDA Setup',
+    //     healthCheckTitle: 'Health Check',
+    //     runHealthCheckButton: 'Run Health Check',
+    //     passedChecks: 'Checks Passed: {0}/{1}',
+    //     statusColumnHeader: 'Status',
+    //     settingColumnHeader: 'Setting',
+    //     descriptionColumnHeader: 'Description',
+    //     recommendedFixColumnHeader: 'Recommended Fix',
+    // }
 }
--- src/lwc/healthCheckHighlightsPanel/healthCheckHighlightsPanel.js
+++ src/lwc/healthCheckHighlightsPanel/healthCheckHighlightsPanel.js
@@ -1,14 +1,12 @@
 import { LightningElement } from 'lwc';
 
+// import custom labels
+import stgEDASetup from '@salesforce/label/c.stgEDASetup';
+import stgSettingsHealthCheckTitle from '@salesforce/label/c.stgSettingsHealthCheckTitle';
+
 export default class HealthCheckHighlightsPanel extends LightningElement {
-    lastRunDate = new Date().toLocaleDateString("en-US");
-
     labelReference = {
-        edaSettingsHealthCheckTitle: 'EDA Settings Health Check', 
-        healthCheckDescription: 'EDA Settings Health Check verifies that all of your EDA Settings are valid and not misconfigured. If any setting configuration is not valid, EDA Settings Health Check will tell you the setting, give a description of which setting is invalid, and how to fix it.'    
-    }
-
-    get lastRunDate(){
-        return this.lastRunDate;
+        stgEDASetup, 
+        stgSettingsHealthCheckTitle    
     }
 }
