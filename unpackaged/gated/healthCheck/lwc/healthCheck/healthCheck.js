16ec477caa268191c7c099769c5379151e740048
m-kinkade <merle.kinkade@salesforce.com>
2020-12-11 11:33:30 -0800

    fixed healthCheck lwc component in unpackaged

--- unpackaged/gated/healthCheck/lwc/healthCheck/healthCheck.js
+++ unpackaged/gated/healthCheck/lwc/healthCheck/healthCheck.js
@@ -1,6 +1,6 @@
 import { LightningElement, track, wire } from 'lwc';
 
-//import getViewModel from '@salesforce/apex/HealthCheckController.getViewModel';
+import getHealthCheckViewModel from '@salesforce/apex/HealthCheckController.getHealthCheckViewModel';
 
 export default class HealthCheck extends LightningElement {
     @track expanded = true;
@@ -9,12 +9,29 @@ export default class HealthCheck extends LightningElement {
     @track lastRunDate = '';
 
     handleHealthCheckRun(){
-        this.lastRunDate = new Date().toLocaleDateString("en-US");
+        var currentDate = new Date();
+        this.lastRunDate = currentDate.toLocaleDateString() + ' ' + currentDate.toLocaleTimeString();
         console.log('Health Check complete!');
     }
 
-    // @wire(getViewModel)
-    // healthCheckViewModel({error, data}){
-    //     console.log('Wiring view model');
-    // }
+    @wire(getHealthCheckViewModel)
+    healthCheckViewModel({error, data}){
+        console.log('Wiring view model');
+
+        if (data){
+            let stringified = JSON.stringify(data);
+            console.log( 'Stringified is ' + stringified );
+
+            let tempData = JSON.parse( stringified );
+            console.log( 'Temp Data is ' + JSON.stringify(tempData) );
+            
+        } else if(error){
+            console.log('Error wiring healthCheckViewModel');
+            if ( Array.isArray( error.body ) )
+                console.log( 'Error is ' + error.body.map( e => e.message ).join( ', ' ) );
+            else if ( typeof error.body.message === 'string' )
+                console.log( 'Error is ' + error.body.message );
+        }
+
+    }
 }
