424b861f422cce405eecc44c487de18435677195
skosgi1 <samsheeth.kosgi@gmail.com>
2020-12-11 14:04:32 -0800

    display date on health check

--- src/lwc/edaSetupHealthCheck/edaSetupHealthCheck.js
+++ src/lwc/edaSetupHealthCheck/edaSetupHealthCheck.js
@@ -8,24 +8,28 @@ export default class HealthCheck extends LightningElement {
     @track passedChecks = 0;
     @track lastRunDate = '';
 
-    handleHealthCheckRun(){
-        var currentDate = new Date();
-        this.lastRunDate = currentDate.toLocaleDateString() + ' ' + currentDate.toLocaleTimeString();
+    handleHealthCheckRun() {
+
+        getHealthCheckViewModel()
+        .then(result => {
+			this.lastRunDate = result.lastRunDate;
+        })
+        
         console.log('Health Check complete!');
     }
 
     @wire(getHealthCheckViewModel)
-    healthCheckViewModel({error, data}){
+    healthCheckViewModel({error, data}) {
         console.log('Wiring view model');
 
-        if (data){
+        if (data) {
             let stringified = JSON.stringify(data);
             console.log( 'Stringified is ' + stringified );
 
             let tempData = JSON.parse( stringified );
             console.log( 'Temp Data is ' + JSON.stringify(tempData) );
             
-        } else if(error){
+        } else if(error) {
             console.log('Error wiring healthCheckViewModel');
             if ( Array.isArray( error.body ) )
                 console.log( 'Error is ' + error.body.map( e => e.message ).join( ', ' ) );
