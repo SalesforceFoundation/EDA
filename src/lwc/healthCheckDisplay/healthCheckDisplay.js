fbfd9fd630945f09a52e8ba13eb4642b8708b057
m-kinkade <merle.kinkade@salesforce.com>
2020-12-21 12:49:02 -0800

    initial commit for iterator in healthCheckDisplay

--- src/lwc/healthCheckDisplay/healthCheckDisplay.js
+++ src/lwc/healthCheckDisplay/healthCheckDisplay.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class HealthCheckDisplay extends LightningElement {}
+export default class HealthCheckDisplay extends LightningElement {
+    @api healthCheckDefinitionsList;
+    @track displayHealthCheckDefinition = false;
+    @track healthCheckGroupCount = 0;
+
+    handleHealthCheckGroupLoaded(){
+        console.log('Health Check Group loaded!');
+        this.healthCheckDefinitionsList++;
+    }
+}
