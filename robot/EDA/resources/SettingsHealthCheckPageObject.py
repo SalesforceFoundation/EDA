ff96942f0cef2a830d5047ecef37b8d71e5d5e03
Rakesh Jeyaraman <rjeyaraman@salesforce.com>
2021-02-18 11:27:37 -0500

    Added health check app robot tests

---
+++ robot/EDA/resources/SettingsHealthCheckPageObject.py
@@ -0,0 +1,43 @@
+from BaseObjects import BaseEDAPage
+from EDA import eda_lex_locators
+from cumulusci.robotframework.pageobjects import HomePage
+from cumulusci.robotframework.pageobjects import pageobject
+import time
+from datetime import date
+
+
+@pageobject("Home", "Settings Health Check")
+class SettingsHealthCheckPage(BaseEDAPage, HomePage):
+
+    def _is_current_page(self):
+        """ Verify we are on the Settings Health Check page
+            by verifying Settings Health Check page header
+        """
+        locator = eda_lex_locators["settings_health_check"]["health_check_header"].format("Settings Health Check")
+        self.selenium.wait_until_page_contains_element(
+            locator,
+            error="Settings Health Check header is not available"
+        )
+
+    def click_health_check_button(self,text):
+        """ This method clicks the 'Run Health Check' button on Settings Health Check page.
+        """
+        locator = eda_lex_locators["settings_health_check"]["run_health_check_button"].format(text)
+        self.selenium.wait_until_page_contains_element(
+            locator, timeout=60, error=f"Run Health Check button with locator '{locator}' is not available")
+        self.selenium.click_element(locator)
+
+    def verify_last_run_date(self):
+        """ This method will verify 'Last Run' field on Settings Health Check page has a date in it
+            and the date is equal to todays date(system date).
+        """
+        locator = eda_lex_locators["settings_health_check"]["last_run_date"]
+        self.selenium.wait_until_page_contains_element(locator, timeout=60, error=f'{locator} is not available')
+        actual_value = self.selenium.get_webelement(locator).text
+        last_run_date = actual_value.split(" ")
+        todays_date = date.today().strftime('%-m/%-d/%Y')
+        self.builtin.log(todays_date)
+        if not todays_date in last_run_date:
+            raise Exception(f"Value of {todays_date} is not present in {last_run_date} and so the dates are not matching as expected")
+
+
---
+++ robot/EDA/tests/browser/health_check/eda_health_check.robot
@@ -0,0 +1,31 @@
+*** Settings ***
+Documentation   Validate health check for EDA
+Resource        robot/EDA/resources/EDA.robot
+Library         cumulusci.robotframework.PageObjects
+...             robot/EDA/resources/SettingsHealthCheckPageObject.py
+
+Suite Setup     Open Test Browser
+Suite Teardown  Capture screenshot and delete records and close browser
+
+*** Test Cases ***
+Verify settings health check app is displayed
+    [Documentation]         Validates 'Settings Health Check" app is displayed under app tile
+    [tags]                  unstable        rbt:high        W-8864633
+    Open app launcher
+    Verify item exists       Settings Health Check
+
+Verify last run date is updated upon clicking run health check Button
+    [Documentation]         Validates the last run date is updated accordingly after clicking the
+    ...                     run health check button.
+    [tags]                  unstable        rbt:high        W-8874163
+    Reload Page
+    Select App Launcher Tab      Settings Health Check
+    Current page should be      Home        Settings Health Check
+    Click health check button       Run Health Check
+    # This sleep is necessary to give some time to the page to update date
+    Sleep       2
+    Verify last run date
+    Go to custom settings setup
+    Select App Launcher Tab      Settings Health Check
+    Verify last run date
+
