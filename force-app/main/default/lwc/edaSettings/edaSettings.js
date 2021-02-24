13536dc70fc9fe5ed52d5ae56aaea3332b052230
skosgi1 <samsheeth.kosgi@gmail.com>
2021-02-24 10:05:15 -0800

    minor changes

--- force-app/main/default/lwc/edaSettings/edaSettings.js
+++ force-app/main/default/lwc/edaSettings/edaSettings.js
@@ -2,7 +2,7 @@ import { LightningElement, api } from 'lwc';
 export default class EDASettings extends LightningElement {
     @api pageReference;
 	LabelReference = {
-        settingsNavigation: 'Navigation Component Here',
-		settingsPage: 'Settings Page Here'
+        settingsNavigation: 'Navigation Pane Here',
+        settingsPage: 'Settings Page Here'
     }
 }
