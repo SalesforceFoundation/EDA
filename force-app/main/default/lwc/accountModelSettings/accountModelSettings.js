850c514e87acb904f6ef00daea4fbe9cc830cb8e
m-kinkade <merle.kinkade@salesforce.com>
2021-02-26 10:29:25 -0800

    initial commit

---
+++ force-app/main/default/lwc/accountModelSettings/accountModelSettings.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import stgAccountModelSettingsTitle from '@salesforce/label/c.stgAccountModelSettingsTitle';
+
+export default class AccountModelSettings extends LightningElement {
+
+    labelReference = {
+        stgAccountModelSettingsTitle
+    }
+    
+}
