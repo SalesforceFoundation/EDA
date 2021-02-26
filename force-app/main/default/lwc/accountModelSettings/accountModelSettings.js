d83e17af496b61e4a2aec089eaf28d9e38a93a19
m-kinkade <merle.kinkade@salesforce.com>
2021-02-26 12:15:53 -0800

    added settings save canvas lwc

--- force-app/main/default/lwc/accountModelSettings/accountModelSettings.js
+++ force-app/main/default/lwc/accountModelSettings/accountModelSettings.js
@@ -1,10 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import stgAccountModelSettingsTitle from '@salesforce/label/c.stgAccountModelSettingsTitle';
 
 export default class AccountModelSettings extends LightningElement {
+    @track isEditMode = false;
+    @track affordancesDisabledToggle = true;
+
+    get affordancesDisabled() {
+        if (affordancesDisabledToggle === true) {
+            return true;
+        } 
+        return undefined;
+    }
 
     labelReference = {
         stgAccountModelSettingsTitle
     }
+
+    handleSettingsEditModeChange(event) {
+        this.isEditMode =!event.detail;
+        this.affordancesDisabledToggle = !event.detail;
+
+        this.refreshAllApex();
+    }
+
+    handleSettingsSaving(event) {
+        console.log('Settings savings button clicked!');
+
+        this.affordancesDisabledToggle = true;
+        this.template.querySeletor("c-settings-save-canvas").updateHierarchySettings();
+    }
     
+    handleSettingsSaveCompleted(event) {
+        console.log('Settings save completed!');
+
+        this.affordancesDisabledToggle = false;
+    }
+
+    refreshAllApex() {
+        console.log('refreshAllApex called');
+    }
 }
---
+++ force-app/main/default/lwc/settingsSaveCanvas/settingsSaveCanvas.js
@@ -0,0 +1,71 @@
+import { LightningElement, api } from 'lwc';
+
+import settingsButtonEdit from '@salesforce/label/c.stgBtnEdit';
+import settingsButtonCancel from '@salesforce/label/c.stgBtnCancel';
+import settingsButtonSave from '@salesforce/label/c.stgBtnSave';
+
+export default class SettingsSaveCanvas extends LightningElement {
+
+    @api componentTitle;
+
+    editButtonShown = true;
+    saveCancelDisabled = undefined;
+
+    hierarchySettingsChanges = {
+        settingsSingleValueBySettingsName: {},
+        settingsListSettingsName: {}
+    }
+
+    labelReference = {
+        settingsButtonEdit,
+        settingsButtonCancel,
+        settingsButtonSave
+    }
+
+    @api
+    handleHierarchySettingsChange(hierarchySettingsChange) {
+        if (hierarchySettingsChange.settingsType === 'string') {
+            hierarchySettingsChanges.settingsSingleValueBySettingsName[hierarchySettingsChange.settingsName] = hierarchySettingsChange.settingsValue;
+        }
+        if (hierarchySettingsChange.settingsType === 'array') {
+            hierarchySettingsChanges.settingsListSettingsName[hierarchySettingsChange.settingsName] = hierarchySettingsChange.settingsValue;
+        }
+    }
+
+    handleEditClick(event) {
+        this.switchEditMode(false);
+        this.dispatchEditModeSwitchEvent();
+    }
+
+    switchEditMode(editMode) {
+        this.editButtonShown = editMode;
+        if (this.editMode === true) {
+            this.saveCancelDisabled = true;
+        } else {
+            this.saveCancelDisabled = undefined;
+        }
+    }
+
+    dispatchEditModeSwitchEvent() {
+        this.dispatchEvent(new CustomEvent('settingseditmodechange', { detail: this.editButtonShown }));
+    }
+
+    handleCancelClick(event) {
+        this.switchEditMode(true);
+        this.clearHierarchySettingsChanges();
+    }
+
+    clearHierarchySettingsChanges() {
+        this.hierarchySettingsChanges.settingsSingleValueBySettingsName = {};
+        this.hierarchySettingsChanges.settingsListSettingsName = {};
+    }
+
+    handleSaveClick() {
+        this.switchEditMode(true);
+        this.dispatchSettingsSavingEvent();
+    }
+
+    dispatchSettingsSavingEvent() {
+        this.dispatchEvent(new CustomEvent('settingssaving', { detail: this.editButtonShown }));
+    }
+}
