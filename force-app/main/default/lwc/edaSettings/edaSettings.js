b0ff2861946e2d65502a7585526928a6b49df269
m-kinkade <merle.kinkade@salesforce.com>
2021-02-26 16:42:40 -0800

    updated EDA settings LWC with changePageToDisplay method

--- force-app/main/default/lwc/edaSettings/edaSettings.js
+++ force-app/main/default/lwc/edaSettings/edaSettings.js
@@ -1,8 +1,23 @@
 import { LightningElement, api } from 'lwc';
 export default class EDASettings extends LightningElement {
     @api pageReference;
+
     labelReference = {
         settingsNavigation: 'Navigation Pane Here',
         settingsPage: 'Settings Page Here'
+    }
+
+    settingsPageToDisplay = {
+        accountModelSettings: true
+    };
+
+    changePageToDisplay(pageName) {
+        for (let settingPageName in this.settingsPageToDisplay) {
+            if (settingPageName.toLocaleLowerCase() === pageName.toLocaleLowerCase()) {
+                this.settingsPageToDisplay[settingPageName] = true;
+            } else {
+                this.settingsPageToDisplay[settingPageName] = false;
+            }
+        }
     }
 }
