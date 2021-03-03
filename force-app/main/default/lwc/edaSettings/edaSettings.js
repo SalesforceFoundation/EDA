67991ecf23304227532999a04ef20893904fe0f7
Xinnan Li <xinnan.li@salesforce.com>
2021-03-03 17:01:50 -0500

    Adding conditional rendering for edaSetting lwc

--- force-app/main/default/lwc/edaSettings/edaSettings.js
+++ force-app/main/default/lwc/edaSettings/edaSettings.js
@@ -1,8 +1,15 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from "lwc";
+import stgErrorInsufficientAccess from "@salesforce/label/c.stgErrorInsufficientAccess";
+import checkAccessForCurrentUser from "@salesforce/apex/EDASettingsController.checkAccessForCurrentUser";
+
 export default class EDASettings extends LightningElement {
     @api pageReference;
     labelReference = {
-        settingsNavigation: 'Navigation Pane Here',
-        settingsPage: 'Settings Page Here'
-    }
+        settingsNavigation: "Navigation Pane Here",
+        settingsPage: "Settings Page Here",
+        stgErrorInsufficientAccess,
+    };
+
+    currentUserHasAccess = false;
+    //@wire(checkAccessForCurrentUser) currentUserHasAccess;
 }
