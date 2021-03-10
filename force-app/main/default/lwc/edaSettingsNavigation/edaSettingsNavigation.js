764b968a7d306e784fb45f8220c0c780df4dbedd
Matthew Blanski <mblanski@salesforce.com>
2021-03-10 16:00:36 -0600

    Added sample structure

--- force-app/main/default/lwc/edaSettingsNavigation/edaSettingsNavigation.js
+++ force-app/main/default/lwc/edaSettingsNavigation/edaSettingsNavigation.js
@@ -9,20 +9,73 @@ export default class EdaSettingsNavigation extends LightningElement {
     settingsNavigationViewModel = {
         navigationSections: [
             {
-                label: this.labelReference.accountModelSettingsTitle,
-                page: "accountmodelsettings",
-                sectionkey: "accountmodelsettings",
-                isActive: true,
+                label: "People and Groups",
+                sectionkey: "peopleandgroups",
+                navigationSubSections: [
+                    {
+                        label: this.labelReference.accountModelSettingsTitle,
+                        page: "accountmodelsettings",
+                        sectionkey: "accountmodelsettings",
+                        menuItems: [
+                            { label: "Default Account Model", sectionKey: "defaultaccountmodel" },
+                            {
+                                label: "Administrative Account Record Type",
+                                sectionKey: "administrativeaccountrecordtype",
+                            },
+                            { label: "Household Account Record Type", sectionKey: "householdaccountrecordtype" },
+                            {
+                                label:
+                                    "Select Account Record Types That Should be Deleted when no Contacts are Related",
+                                sectionKey: "autodeleterecordtypes",
+                            },
+                            { label: "Administrative Account Naming", sectionKey: "administrativeaccountnaming" },
+                            { label: "Household Account Naming", sectionKey: "householdAccountNaming" },
+                            { label: "Rename Household Accounts", sectionKey: "renamehouseholdaccounts" },
+                        ],
+                        isActive: true,
+                    },
+                    {
+                        label: "Contact Information",
+                        page: "contactinfosettings",
+                        sectionkey: "contactinfosettings",
+                        menuItems: [
+                            { label: "Default Contact Language Fluency", sectionKey: "defaultcontactlanguagefluency" },
+                            { label: "Require Preferred Email", sectionKey: "requirepreferredemail" },
+                            { label: "Enhanced Preferred Phone Functionality", sectionKey: "enhancedpreferredphone" },
+                            { label: "Default Preferred Phone", sectionKey: "defaultpreferredphone" },
+                        ],
+                    },
+                    {
+                        label: "Addresses",
+                        page: "addresssettings",
+                        sectionkey: "addresssettings",
+                        menuItems: [
+                            { label: "Allow Multiple Contact Addresses", sectionKey: "contactmultipleaddresses" },
+                            {
+                                label: "Enable Multiple Addresses for Account Types",
+                                sectionKey: "addressaccountrecordtypes",
+                            },
+                            {
+                                label: "Update Address Records for Simple Address Changes",
+                                sectionKey: "simpleaddresschangeisupdate",
+                            },
+                        ],
+                    },
+                ],
             },
             {
-                label: "Contact Information",
-                page: "contactinfosettings",
-                sectionkey: "contactinfosettings",
+                label: "Affiliations",
+                page: "affiliations",
+                sectionkey: "affiliations",
+                menuItems: [{ label: "Enforce Record Type Validation", sectionKey: "enforcerecordtypevalidation" }],
             },
             {
-                label: "Addresses",
-                page: "addresssettings",
-                sectionkey: "addresssettings",
+                label: "Courses and Enrollments",
+                sectionkey: "coursesandenrollments",
+            },
+            {
+                label: "System Tools",
+                sectionkey: "systemtools",
             },
         ],
     };
