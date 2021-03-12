b3fe263a4d8e74e92e2ca95fa4f93c75a65c274f
m-kinkade <merle.kinkade@salesforce.com>
2021-03-12 09:46:57 -0800

    added functions to handle account model changes in js controller

--- force-app/main/default/lwc/accountModelSettings/accountModelSettings.js
+++ force-app/main/default/lwc/accountModelSettings/accountModelSettings.js
@@ -1,5 +1,14 @@
-import { LightningElement, api, track } from "lwc";
+import { LightningElement, api, track, wire } from "lwc";
+
+import getAccountModelSettingsViewModel from "@salesforce/apex/AccountModelSettingsController.getAccountModelSettingsViewModel";
+
 import stgAccountModelSettingsTitle from "@salesforce/label/c.stgAccountModelSettingsTitle";
+import stgAccModelTitle from "@salesforce/label/c.stgAccModelTitle";
+import stgHelpAccountModel from "@salesforce/label/c.stgHelpAccountModel";
+import stgAdminAccountRecordType from "@salesforce/label/c.stgAdminAccountRecordType";
+import stgHelpAdminRecType from "@salesforce/label/c.stgHelpAdminRecType";
+import stgAccountRecordTypeSupportsHHAddress from "@salesforce/label/c.stgAccountRecordTypeSupportsHHAddress";
+import stgHelpHouseholdRecType from "@salesforce/label/c.stgHelpHouseholdRecType";
 
 export default class AccountModelSettings extends LightningElement {
     isEditMode = false;
@@ -14,7 +23,27 @@ export default class AccountModelSettings extends LightningElement {
 
     labelReference = {
         stgAccountModelSettingsTitle,
+        stgAccModelTitle,
+        stgHelpAccountModel,
+        stgAdminAccountRecordType,
+        stgHelpAdminRecType,
+        stgAccountRecordTypeSupportsHHAddress,
+        stgHelpHouseholdRecType,
     };
+
+    @wire(getAccountModelSettingsViewModel) accountModelSettingsViewModel;
+
+    handleDefaultAccountModelChange(event) {
+        console.log("Default Account model changed");
+    }
+
+    handleHouseholdAccountModelChange(event) {
+        console.log("HH Account model changed");
+    }
+
+    handleAdministrativeAccountModelChange(event) {
+        console.log("Admin Account model changed");
+    }
 
     handleSettingsEditModeChange(event) {
         this.isEditMode = !event.detail;
