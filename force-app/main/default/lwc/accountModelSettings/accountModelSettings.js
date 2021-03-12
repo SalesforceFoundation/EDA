d207def1b9837ac4831c4c1fc2a2f33e117d0da3
Matthew Blanski <mblanski@salesforce.com>
2021-03-12 08:28:56 -0600

    returned affordances disabled to false

--- force-app/main/default/lwc/accountModelSettings/accountModelSettings.js
+++ force-app/main/default/lwc/accountModelSettings/accountModelSettings.js
@@ -1,37 +1,36 @@
-import { LightningElement, api, track } from 'lwc';
-import stgAccountModelSettingsTitle from '@salesforce/label/c.stgAccountModelSettingsTitle';
+import { LightningElement, api, track } from "lwc";
+import stgAccountModelSettingsTitle from "@salesforce/label/c.stgAccountModelSettingsTitle";
 
 export default class AccountModelSettings extends LightningElement {
     isEditMode = false;
-    affordancesDisabledToggle = true;
+    affordancesDisabledToggle = false;
 
     get affordancesDisabled() {
         if (!this.isEditMode || this.affordancesDisabledToggle === true) {
             return true;
-        } 
+        }
         return undefined;
     }
 
     labelReference = {
-        stgAccountModelSettingsTitle
-    }
+        stgAccountModelSettingsTitle,
+    };
 
-    handleSettingsEditModeChange(event) { 
-        this.isEditMode =!event.detail;
+    handleSettingsEditModeChange(event) {
+        this.isEditMode = !event.detail;
         this.affordancesDisabledToggle = event.detail;
 
         this.refreshAllApex();
     }
 
-    handleSettingsSaving(event) { 
+    handleSettingsSaving(event) {
         this.affordancesDisabledToggle = true;
-        this.template.querySelector('c-settings-save-canvas').updateHierarchySettings();
+        this.template.querySelector("c-settings-save-canvas").updateHierarchySettings();
     }
-    
+
     handleSettingsSaveCompleted(event) {
         this.affordancesDisabledToggle = false;
     }
 
-    refreshAllApex() {
-    }
+    refreshAllApex() {}
 }
