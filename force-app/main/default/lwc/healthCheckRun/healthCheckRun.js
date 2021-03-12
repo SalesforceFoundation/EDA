dda75623ce5392fe3dca990955c46126776917f1
Xinnan Li <xinnan.li@salesforce.com>
2021-03-12 14:22:48 -0500

    ignore json and run prettier against healthcheck

--- force-app/main/default/lwc/healthCheckRun/healthCheckRun.js
+++ force-app/main/default/lwc/healthCheckRun/healthCheckRun.js
@@ -1,29 +1,23 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 // import custom labels
-import stgHealthCheckTitle from '@salesforce/label/c.stgHealthCheckTitle';
-import stgHealthCheckDescription from '@salesforce/label/c.stgHealthCheckDescription';
-import stgHealthCheckLastRun from '@salesforce/label/c.stgHealthCheckLastRun';
-import stgHealthCheckRunButton from '@salesforce/label/c.stgHealthCheckRunButton';
+import stgHealthCheckTitle from "@salesforce/label/c.stgHealthCheckTitle";
+import stgHealthCheckDescription from "@salesforce/label/c.stgHealthCheckDescription";
+import stgHealthCheckLastRun from "@salesforce/label/c.stgHealthCheckLastRun";
+import stgHealthCheckRunButton from "@salesforce/label/c.stgHealthCheckRunButton";
 
 export default class HealthCheckActionCard extends LightningElement {
     @api runDate;
 
-    labelReference = {stgHealthCheckTitle,
-        stgHealthCheckDescription,
-        stgHealthCheckLastRun,
-        stgHealthCheckRunButton
-    }
+    labelReference = { stgHealthCheckTitle, stgHealthCheckDescription, stgHealthCheckLastRun, stgHealthCheckRunButton };
 
     get lastRunDate() {
-        if (this.runDate){
-            return this.labelReference.stgHealthCheckLastRun.replace(
-                '{0}', this.runDate
-            );
+        if (this.runDate) {
+            return this.labelReference.stgHealthCheckLastRun.replace("{0}", this.runDate);
         }
     }
 
-    runHealthCheck(event){
-        this.dispatchEvent(new CustomEvent('runhealthcheck'));
+    runHealthCheck(event) {
+        this.dispatchEvent(new CustomEvent("runhealthcheck"));
     }
 }
