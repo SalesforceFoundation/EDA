554a453c62ffc49f852b13b4eca7ab264eee78ad
Matthew Blanski <mblanski@salesforce.com>
2021-03-30 09:58:27 -0500

    Converted all nav to labels

--- force-app/main/default/lwc/edaSettingsNavigation/edaSettingsNavigation.js
+++ force-app/main/default/lwc/edaSettingsNavigation/edaSettingsNavigation.js
@@ -1,25 +1,54 @@
 import { LightningElement, api, track } from "lwc";
-import accountModelSettingsTitle from "@salesforce/label/c.stgAccountModelSettingsTitle";
-import stgAffiliationsSettingsNav from "@salesforce/label/c.stgAffiliationsSettingsNav";
-import afflTypeEnforced from "@salesforce/label/c.afflTypeEnforced";
+import stgAccountModelSettingsTitle from "@salesforce/label/c.stgAccountModelSettingsTitle";
+import stgAccModelTitle from "@salesforce/label/c.stgAccModelTitle";
+import stgAdminAccountRecordType from "@salesforce/label/c.stgAdminAccountRecordType";
+import stgAccountRecordTypeSupportsHHAddress from "@salesforce/label/c.stgAccountRecordTypeSupportsHHAddress";
+import stgAccoutTypesWithoutContactsDelete from "@salesforce/label/c.stgAccoutTypesWithoutContactsDelete";
+
+import stgContactInformationSettingsTitle from "@salesforce/label/c.stgContactInformationSettingsTitle";
+import stgDefaultContactLanguageFluency from "@salesforce/label/c.stgDefaultContactLanguageFluency";
+import stgDisablePreferredEmailEnforcement from "@salesforce/label/c.stgDisablePreferredEmailEnforcement";
+import stgEnablePreferredPhoneSync from "@salesforce/label/c.stgEnablePreferredPhoneSync";
+import stgPreferredPhoneDefault from "@salesforce/label/c.stgPreferredPhoneDefault";
+
 import stgAccountTypesMultiAddressesEnabled from "@salesforce/label/c.stgAccountTypesMultiAddressesEnabled";
 import stgAddressSettingsTitle from "@salesforce/label/c.stgAddressSettingsTitle";
-import stgAffiliationsSettingsTitle from "@salesforce/label/c.stgAffiliationsSettingsTitle";
 import stgContactMultiAddressesEnabled from "@salesforce/label/c.stgContactMultiAddressesEnabled";
 import stgHealthCheckLoadingIndicator from "@salesforce/label/c.stgHealthCheckLoadingIndicator";
 import stgSimpleAddressChangeUpdate from "@salesforce/label/c.stgSimpleAddressChangeUpdate";
+import stgTabAfflMappings from "@salesforce/label/c.stgTabAfflMappings";
+
+import stgAffiliationsSettingsNav from "@salesforce/label/c.stgAffiliationsSettingsNav";
+import afflTypeEnforced from "@salesforce/label/c.afflTypeEnforced";
 
 export default class EdaSettingsNavigation extends LightningElement {
     labelReference = {
-        accountModelSettingsTitle,
-        stgAffiliationsSettingsNav,
-        afflTypeEnforced,
         spinnerLoadingAltText: stgHealthCheckLoadingIndicator,
-        stgAffiliationsSettingsTitle,
-        stgAccountTypesMultiAddressesEnabled,
-        stgAddressSettingsTitle,
-        stgContactMultiAddressesEnabled,
-        stgSimpleAddressChangeUpdate,
+        accountModel: {
+            accountAutoDeletionModel: stgAccoutTypesWithoutContactsDelete,
+            accountModelSettings: stgAccountModelSettingsTitle,
+            adminAccountModel: stgAdminAccountRecordType,
+            defaultAccountModel: stgAccModelTitle,
+            hhAccountModel: stgAccountRecordTypeSupportsHHAddress,
+        },
+        contactInformation: {
+            contactInformation: stgContactInformationSettingsTitle,
+            defaultContactLanguageFluency: stgDefaultContactLanguageFluency,
+            defaultPreferredPhone: stgPreferredPhoneDefault,
+            enhancedPhoneFunctionality: stgEnablePreferredPhoneSync,
+            requirePreferredEmail: stgDisablePreferredEmailEnforcement,
+        },
+        addressSettings: {
+            addressAccountRecordTypes: stgAccountTypesMultiAddressesEnabled,
+            addressSettings: stgAddressSettingsTitle,
+            contactMultipleAddresses: stgContactMultiAddressesEnabled,
+            simpleAddressChangeIsUpdate: stgSimpleAddressChangeUpdate,
+        },
+        affiliationSettings: {
+            affiliationMappings: stgTabAfflMappings,
+            affiliationSettings: stgAffiliationsSettingsNav,
+            enforceRecordTypeValidation: afflTypeEnforced,
+        },
     };
 
     @api activePage;
@@ -36,20 +65,19 @@ export default class EdaSettingsNavigation extends LightningElement {
                 id: "peopleAndGroups",
                 navigationSubSections: [
                     {
-                        label: this.labelReference.accountModelSettingsTitle,
+                        label: this.labelReference.accountModel.accountModelSettings,
                         page: "accountModelSettings",
                         id: "accountModelSettings",
                         menuItems: [
-                            { label: "Default Account Model", id: "defaultaccountmodel" },
+                            { label: this.labelReference.accountModel.defaultAccountModel, id: "defaultAccountModel" },
                             {
-                                label: "Administrative Account Record Type",
-                                id: "administrativeAccountRecordType",
+                                label: this.labelReference.accountModel.adminAccountModel,
+                                id: "adminAccountModel",
                             },
-                            { label: "Household Account Record Type", id: "householdAccountRecordType" },
+                            { label: this.labelReference.accountModel.hhAccountModel, id: "hhAccountModel" },
                             {
-                                label:
-                                    "Select Account Record Types That Should be Deleted when no Contacts are Related",
-                                id: "AutodeleteRecordTypes",
+                                label: this.labelReference.accountModel.accountAutoDeletionModel,
+                                id: "accountAutoDeletionModel",
                             },
                             { label: "Administrative Account Naming", id: "administrativeAccountNaming" },
                             { label: "Household Account Naming", id: "householdAccountNaming" },
@@ -58,28 +86,43 @@ export default class EdaSettingsNavigation extends LightningElement {
                         isActive: true,
                     },
                     {
-                        label: "Contact Information",
+                        label: this.labelReference.contactInformation.contactInformation,
                         page: "contactInformationSettings",
                         id: "contactInformation",
                         menuItems: [
-                            { label: "Default Contact Language Fluency", id: "defaultContactLanguageFluency" },
-                            { label: "Require Preferred Email", id: "requirePreferredEmail" },
-                            { label: "Enhanced Preferred Phone Functionality", id: "enhancedPreferredPhone" },
-                            { label: "Default Preferred Phone", id: "defaultPreferredPhone" },
+                            {
+                                label: this.labelReference.contactInformation.defaultContactLanguageFluency,
+                                id: "defaultContactLanguageFluency",
+                            },
+                            {
+                                label: this.labelReference.contactInformation.requirePreferredEmail,
+                                id: "requirePreferredEmail",
+                            },
+                            {
+                                label: this.labelReference.contactInformation.enhancedPhoneFunctionality,
+                                id: "enhancedPhoneFunctionality",
+                            },
+                            {
+                                label: this.labelReference.contactInformation.defaultPreferredPhone,
+                                id: "defaultPreferredPhone",
+                            },
                         ],
                     },
                     {
-                        label: stgAddressSettingsTitle,
+                        label: this.labelReference.addressSettings.addressSettings,
                         page: "addressSettings",
                         id: "addresses",
                         menuItems: [
-                            { label: stgContactMultiAddressesEnabled, id: "contactMultipleAddresses" },
                             {
-                                label: stgAccountTypesMultiAddressesEnabled,
+                                label: this.labelReference.addressSettings.contactMultipleAddresses,
+                                id: "contactMultipleAddresses",
+                            },
+                            {
+                                label: this.labelReference.addressSettings.simpleAddressChangeIsUpdate,
                                 id: "addressAccountRecordTypes",
                             },
                             {
-                                label: stgSimpleAddressChangeUpdate,
+                                label: this.labelReference.addressSettings.addressAccountRecordTypes,
                                 id: "simpleAddressChangeIsUpdate",
                             },
                         ],
@@ -87,10 +130,19 @@ export default class EdaSettingsNavigation extends LightningElement {
                 ],
             },
             {
-                label: stgAffiliationsSettingsNav,
+                label: this.labelReference.affiliationSettings.affiliationSettings,
                 page: "affiliationSettings",
                 id: "affiliations",
-                menuItems: [{ label: afflTypeEnforced, id: "enforceRecordTypeValidation" }],
+                menuItems: [
+                    {
+                        label: this.labelReference.affiliationSettings.affiliationMappings,
+                        id: "enforceRecordTypeValidation",
+                    },
+                    {
+                        label: this.labelReference.affiliationSettings.enforceRecordTypeValidation,
+                        id: "affiliationMappings",
+                    },
+                ],
             },
             {
                 label: "Courses and Enrollments",
