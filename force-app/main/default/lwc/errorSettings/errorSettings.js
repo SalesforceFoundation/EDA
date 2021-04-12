0e4860f62e013476eccfc9a7a5d9290b378b0efc
m-kinkade <merle.kinkade@salesforce.com>
2021-04-12 16:50:44 -0700

    added front end

--- force-app/main/default/lwc/errorSettings/errorSettings.js
+++ force-app/main/default/lwc/errorSettings/errorSettings.js
@@ -8,6 +8,8 @@ import stgSystemSettingsTitle from "@salesforce/label/c.stgSystemSettingsTitle";
 import stgErrorSettingsTitle from "@salesforce/label/c.stgErrorSettingsTitle";
 import stgStoreErrorsTitle from "@salesforce/label/c.stgStoreErrorsTitle";
 import stgHelpStoreErrorsOn from "@salesforce/label/c.stgHelpStoreErrorsOn";
+import stgDisableErrorHandlingTitle from "@salesforce/label/c.stgDisableErrorHandlingTitle";
+import stgHelpErrorDisable from "@salesforce/label/c.stgHelpErrorDisable";
 
 export default class ErrorSettings extends LightningElement {
     isEditMode = false;
@@ -22,10 +24,13 @@ export default class ErrorSettings extends LightningElement {
         errorSettingsTitle: stgErrorSettingsTitle,
         storeErrorsSettingTitle: stgStoreErrorsTitle,
         storeErrorsSettingDescription: stgHelpStoreErrorsOn,
+        errorHandlingSettingTitle: stgDisableErrorHandlingTitle,
+        errorHandlingSettingDescription: stgHelpErrorDisable,
     };
 
     inputAttributeReference = {
         storeErrorsToggleId: "storeErrors",
+        errorHandlingToggleId: "errorHandling",
     };
 
     get affordancesDisabled() {
@@ -58,6 +63,18 @@ export default class ErrorSettings extends LightningElement {
         this.template.querySelector("c-settings-save-canvas").handleHierarchySettingsChange(hierarchySettingsChange);
     }
 
+    handleErrorHandlingChange(event) {
+        const eventDetail = event.detail;
+
+        let hierarchySettingsChange = {
+            settingsType: "boolean",
+            settingsName: "Disable_Error_Handling__c",
+            settingsValue: !eventDetail.value, // UI input should display inverse of value specified in hierarchy settings for this field
+        };
+
+        this.template.querySelector("c-settings-save-canvas").handleHierarchySettingsChange(hierarchySettingsChange);
+    }
+
     handleSettingsEditModeChange(event) {
         this.isEditMode = !event.detail;
         this.affordancesDisabledToggle = event.detail;
@@ -80,6 +97,7 @@ export default class ErrorSettings extends LightningElement {
 
     handleSettingsSaveCompleted(event) {
         this.affordancesDisabledToggle = false;
+        console.log("refreshing");
         this.refreshAllApex();
     }
 
