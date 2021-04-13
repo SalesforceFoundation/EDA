6fed5e44167e96f93f6cc958b8a1dbc61d831793
skosgi1 <samsheeth.kosgi@gmail.com>
2021-04-13 12:38:28 -0700

    ctrl, srvc

--- force-app/main/default/lwc/systemTools/systemTools.js
+++ force-app/main/default/lwc/systemTools/systemTools.js
@@ -2,9 +2,30 @@ import { LightningElement, wire, track } from "lwc";
 import { refreshApex } from "@salesforce/apex";
 
 import stgSystemToolsTitle from "@salesforce/label/c.stgSystemToolsTitle";
+import stgBtnRefreshHouseholdNames from "@salesforce/label/c.stgBtnRefreshHouseholdNames";
+import stgRefreshHHAcctNameTitle from "@salesforce/label/c.stgRefreshHHAcctNameTitle";
+import stgRefreshHHAcctNameDesc from "@salesforce/label/c.stgRefreshHHAcctNameDesc";
 
 export default class systemTools extends LightningElement {
     labelReference = {
         stgSystemToolsTitle: stgSystemToolsTitle,
+        stgBtnRefreshHouseholdNames: stgBtnRefreshHouseholdNames,
+        stgRefreshHHAcctNameTitle: stgRefreshHHAcctNameTitle,
+        stgRefreshHHAcctNameDesc: stgRefreshHHAcctNameDesc,
     };
+
+    handleRefreshHHNamesBtnClick(event) {
+        const eventDetail = event.detail;
+        this.dispatchSettingsBatchJobModalEvent(eventDetail);
+    }
+
+    dispatchSettingsBatchJobModalEvent(eventDetail) {
+        this.dispatchEvent(
+            new CustomEvent("settingsbatchjobmodalrequestevent", {
+                detail: event.detail,
+                bubbles: true,
+                composed: true,
+            })
+        );
+    }
 }
