99eeeea8023a82b5309deb34a9ee1c220ceb092e
skosgi1 <samsheeth.kosgi@gmail.com>
2021-04-15 15:21:25 -0700

    removing comments

--- force-app/main/default/aura/batchJobModal/batchJobModalController.js
+++ force-app/main/default/aura/batchJobModal/batchJobModalController.js
@@ -1,8 +1,1 @@
-({
-    // handleAccountRecordTypeChange: function (component, event, helper) {
-    //     helper.handleAccountRecordTypeChange(component, event.getParam("accountRecordType"));
-    // },
-    // handleContactFieldChange: function (component, event, helper) {
-    //     helper.handleContactFieldChange(component, event.getParam("contactField"));
-    // }
-});
+({});
