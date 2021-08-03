adbc9c6f21df7b31ff877c8b3e67961a1ee36a9f
Salvador Mora <salvador.mora@salesforce.com>
2021-08-03 15:06:28 -0700

    Refactored educationCloudSettings component to use a display component to orchestrate the load of edcSettingsProductCard child components.

---
+++ force-app/main/educationCloudSettings/lwc/edcSettingsProductDisplay/edcSettingsProductDisplay.js
@@ -0,0 +1,48 @@
+import { LightningElement, api, track } from 'lwc';
+import stgHealthCheckLoadingIndicator from "@salesforce/label/c.stgHealthCheckLoadingIndicator";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+
+export default class EdcSettingsProductDisplay extends LightningElement {
+    @api productRegistryVModels;
+
+    @track loadedSettingsProductCount = 0;
+    @track displaySettingsProduct = false;
+    errorList = [];
+
+    LabelReference = {
+        spinnerLoadingAltText: stgHealthCheckLoadingIndicator,
+    };
+
+    handleSettingsProductLoaded(evt) {
+        this.loadedSettingsProductCount++;
+        this.areAllProductsLoaded();
+    }
+
+    handleSettingsProductError(evt){
+        const errorMessage = evt.detail.errorMessage;
+        this.errorList.push(errorMessage);
+        this.loadedSettingsProductCount++;
+        this.areAllProductsLoaded();
+    }
+
+    areAllProductsLoaded(){
+        if (this.loadedSettingsProductCount == this.productRegistryVModels.length) {
+            this.displaySettingsProduct = true;
+            if (this.errorList && this.errorList.length > 0){
+                this.errorList.forEach(error => {
+                    this.createShowToast(error);
+                });
+            }
+        }
+    }
+
+    createShowToast(errorMessage) {
+        const evt = new ShowToastEvent({
+            title: 'Error',
+            message: errorMessage,
+            variant: 'error',
+            mode: 'sticky'
+        });
+        this.dispatchEvent(evt);
+    }
+}
