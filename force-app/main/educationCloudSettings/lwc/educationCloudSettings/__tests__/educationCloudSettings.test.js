f937c364fba9edb7b9b59951c24ba75531c84cc0
Salvador Mora <salvador.mora@salesforce.com>
2021-08-05 13:29:14 -0700

    Modified test classes to use error message labels Added new test method for handling bad response from product information Added Jest test method for showing the edcSettingsProductDisplay element

--- force-app/main/educationCloudSettings/lwc/educationCloudSettings/__tests__/educationCloudSettings.test.js
+++ force-app/main/educationCloudSettings/lwc/educationCloudSettings/__tests__/educationCloudSettings.test.js
@@ -1,10 +1,32 @@
 import { createElement } from "lwc";
 import EducationCloudSettings from "c/educationCloudSettings";
+import getProductRegistrySettingsProductInformationVModels from "@salesforce/apex/EducationCloudSettingsController.getProductRegistrySettingsProductInformationVModels";
+
+// Import mock data for c-edc-settings-product-display
+const mockGetProductRegistrySettingsProductInformationVModels = require("./data/getProductRegistrySettingsProductInformationVModels.json");
+
+jest.mock(
+    '@salesforce/apex/EducationCloudSettingsController.getProductRegistrySettingsProductInformationVModels',
+    () => {
+        const {
+            createApexTestWireAdapter
+        } = require('@salesforce/sfdx-lwc-jest');
+        return {
+            default: createApexTestWireAdapter(jest.fn())
+        };
+    },
+    { virtual: true }
+);
+
 
 describe("c-education-cloud-settings", () => {
     afterEach(() => {
         clearDOM();
     });
+
+    async function flushPromises() {
+        return Promise.resolve();
+    }
 
     it("Check if release management tile is visible and navigation target is correct", () => {
         const element = createElement("c-education-cloud-settings", {
@@ -23,6 +45,45 @@ describe("c-education-cloud-settings", () => {
             const containerName = releaseManagementCardElement.navigationTarget;
             expect(containerName).not.toBeNull();
             expect(containerName.endsWith("releaseManagementContainer")).toBeTruthy();
+        });
+    });
+
+    it("Check if EDC Settings Product Display is shown and it has the parameter data", async() => {
+        const element = createElement("c-education-cloud-settings", {
+            is: EducationCloudSettings,
+        });
+
+        document.body.appendChild(element);
+        getProductRegistrySettingsProductInformationVModels.emit(mockGetProductRegistrySettingsProductInformationVModels);
+
+        await flushPromises();
+
+        const edcSettingsProductCardElement = element.shadowRoot.querySelector(
+            "c-edc-settings-product-display"
+        );
+        expect(edcSettingsProductCardElement).not.toBeNull();
+        const productRegistryVModels = edcSettingsProductCardElement.productRegistryVModels;
+        expect(productRegistryVModels).not.toBeNull();
+        expect(productRegistryVModels.length).toBe(2);
+
+        productRegistryVModels.forEach(productRegistry=> {
+            const action = productRegistry.action;
+            const namespace = productRegistry.namespace;
+            const classname = productRegistry.classname;
+            const apiVersion = productRegistry.apiVersion;
+
+            if(action === 'testAction1') {
+                expect(namespace).toBe('testNamespace1');
+                expect(classname).toBe('testClassname1');
+                expect(apiVersion).toBe(1);
+                return;
+            }
+            if(action === 'testAction2') {
+                expect(namespace).toBe('testNamespace2');
+                expect(classname).toBe('testClassname2');
+                expect(apiVersion).toBe(2);
+                return;
+            }
         });
     });
 });
