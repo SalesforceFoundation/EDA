9ba066516164cc098437fd3a5ebee531344b9d9c
Iskander <imukhamedgaliyev@salesforce.com>
2021-08-09 15:12:43 -0400

    UI, mock callable class, unit tests

--- force-app/main/releaseGating/lwc/releaseGateItem/releaseGateItem.js
+++ force-app/main/releaseGating/lwc/releaseGateItem/releaseGateItem.js
@@ -1,3 +1,85 @@
-import { LightningElement } from "lwc";
+import { LightningElement, track, wire, api } from "lwc";
+import stgReleaseGateActivateBy from "@salesforce/label/c.stgReleaseGateActivateBy";
+import stgReleaseGateActivatedOn from "@salesforce/label/c.stgReleaseGateActivatedOn";
+import stgReleaseGateActivate from "@salesforce/label/c.stgReleaseGateActivate";
+import stgBtnReleaseGateActivate from "@salesforce/label/c.stgBtnReleaseGateActivate";
+import stgReleaseGateInProgress from "@salesforce/label/c.stgReleaseGateInProgress";
+import stgReleaseGateDueDateInfoText from "@salesforce/label/c.stgReleaseGateDueDateInfoText";
+import stgNewWindowA11y from "@salesforce/label/c.stgNewWindowA11y";
 
-export default class ReleaseGateItem extends LightningElement {}
+export default class ReleaseGateItem extends LightningElement {
+    @api productRegistryName;
+    @api product;
+    @api gate;
+
+    labelReference = {
+        releaseGateActivateBy: stgReleaseGateActivateBy,
+        releaseGateActivatedOn: stgReleaseGateActivatedOn,
+        releaseGateActivate: stgReleaseGateActivate,
+        releaseGateActivateButton: stgBtnReleaseGateActivate,
+        releaseGateInProgress: stgReleaseGateInProgress,
+        newWindowLinkAlt: stgNewWindowA11y,
+        releaseGateDueDateInfoText: stgReleaseGateDueDateInfoText,
+    };
+
+    get gateActive() {
+        return this.gate.status === "active";
+    }
+
+    get gateInactive() {
+        return this.gate.status === "inactive";
+    }
+
+    get gateDisabled() {
+        return this.gate.status === "disabled";
+    }
+
+    get gateDisabledOrInactive() {
+        return this.gateDisabled || this.gateInactive;
+    }
+
+    get gateInProgress() {
+        return this.gate.status === "inprogress";
+    }
+
+    get activatedOnLabel() {
+        return this.labelReference.releaseGateActivatedOn.replace(
+            "{0}",
+            new Date(this.gate.activationDate).toLocaleString()
+        );
+    }
+
+    get inProgressLabel() {
+        return this.labelReference.releaseGateInProgress.replace("{0}", this.gate.label);
+    }
+
+    get activateLabel() {
+        return this.labelReference.releaseGateActivate.replace("{0}", this.gate.label);
+    }
+
+    get approvedAltText() {
+        return this.activatedOnLabel;
+    }
+
+    get hasDueDate() {
+        return this.gate.dueDate && this.gateInactive;
+    }
+
+    get hasActivationDate() {
+        return this.gate.activationDate && this.gateActive;
+    }
+
+    handleEnableGate(event) {
+        let payload = {
+            productRegistryName: this.productRegistryName,
+            product: this.product,
+            gate: this.gate,
+        };
+
+        const enableEvent = new CustomEvent("enablegate", {
+            detail: payload,
+        });
+
+        this.dispatchEvent(enableEvent);
+    }
+}
--- force-app/main/releaseGating/lwc/releaseGateProduct/releaseGateProduct.js
+++ force-app/main/releaseGating/lwc/releaseGateProduct/releaseGateProduct.js
@@ -1,7 +1,20 @@
 import { LightningElement, track, wire, api } from "lwc";
-import getProductRegistryReleaseGateVModels from "@salesforce/apex/ReleaseGateController.getProductRegistryReleaseGateVModels";
+import getReleaseGateVModel from "@salesforce/apex/ReleaseGateController.getReleaseGateVModel";
 
 export default class ReleaseGateProduct extends LightningElement {
     @api
     productRegistryName;
+
+    @track releaseGateVModel;
+    @track releaseGateVModelWireResult;
+
+    @wire(getReleaseGateVModel, { productRegistryName: "$productRegistryName" })
+    releaseGateVModelWire(result) {
+        this.releaseGateVModelWireResult = result;
+        if (result.data) {
+            this.releaseGateVModel = result.data;
+        } else if (result.error) {
+            //console.log("error retrieving releaseGateVModel");
+        }
+    }
 }
