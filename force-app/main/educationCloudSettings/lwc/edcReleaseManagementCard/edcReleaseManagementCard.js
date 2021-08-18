d25bf1ea36f9ad3a2c13583388cd1da2779752ef
Matthew Blanski <mblanski@salesforce.com>
2021-08-18 11:07:14 -0500

    tweaks

--- force-app/main/educationCloudSettings/lwc/edcReleaseManagementCard/edcReleaseManagementCard.js
+++ force-app/main/educationCloudSettings/lwc/edcReleaseManagementCard/edcReleaseManagementCard.js
@@ -9,6 +9,7 @@ import stgBtnReleaseManagementA11y from "@salesforce/label/c.stgBtnReleaseManage
 
 export default class EdcReleaseManagementCard extends NavigationMixin(LightningElement) {
     @api navigationTarget;
+    alarm = false;
 
     labelReference = {
         releaseManagementButton: stgBtnReleaseManagement,
@@ -32,5 +33,13 @@ export default class EdcReleaseManagementCard extends NavigationMixin(LightningE
             },
         };
         this[NavigationMixin.Navigate](pageReference);
+    }
+
+    get notificationIcon() {
+        if (this.alarm) {
+            return "utility:notification";
+        }
+
+        return "utility:alert";
     }
 }
