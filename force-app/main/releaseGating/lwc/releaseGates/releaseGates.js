341a6625564332a704ff1f864b20fdb5f2e8edd0
Iskander <imukhamedgaliyev@salesforce.com>
2021-08-20 13:30:12 -0400

    Activate release gate labels and unit tests

--- force-app/main/releaseGating/lwc/releaseGates/releaseGates.js
+++ force-app/main/releaseGating/lwc/releaseGates/releaseGates.js
@@ -1,9 +1,12 @@
 import { LightningElement, track, wire, api } from "lwc";
 import { refreshApex } from "@salesforce/apex";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+//Release gate labels
 import stgReleaseGateInProgress from "@salesforce/label/c.stgReleaseGateInProgress";
 import stgReleaseGateActivateSuccess from "@salesforce/label/c.stgReleaseGateActivateSuccess";
+import stgReleaseGateActivateInProgress from "@salesforce/label/c.stgReleaseGateActivateInProgress";
 import stgSuccess from "@salesforce/label/c.stgSuccess";
+//Apex methods
 import getProductRegistryReleaseGateVModels from "@salesforce/apex/ReleaseGateController.getProductRegistryReleaseGateVModels";
 import activateReleaseGate from "@salesforce/apex/ReleaseGateController.activateReleaseGate";
 
@@ -13,8 +16,9 @@ export default class ReleaseGates extends LightningElement {
 
     labelReference = {
         releaseGateActivateSuccess: stgReleaseGateActivateSuccess,
-        releaseGateInProgress: stgReleaseGateInProgress,
-        success: stgSuccess,
+        releaseGateActivateInProgress: stgReleaseGateActivateInProgress,
+        inProgressTitle: stgReleaseGateInProgress,
+        successTitle: stgSuccess,
     };
 
     @wire(getProductRegistryReleaseGateVModels)
@@ -53,14 +57,15 @@ export default class ReleaseGates extends LightningElement {
                 if (result) {
                     let message, title, toastType, toastMode;
                     let releaseGateLabel = saveModel.productLabel + " " + saveModel.releaseGateLabel;
-                    if (result.some((activateResult) => activateResult.status === "inprogress")) {
-                        title = this.labelReference.releaseGateInProgress.replace("{0}", releaseGateLabel);
-                        message = "";
+                    let isInProgress = result.some((activateResult) => activateResult.status === "inprogress");
+                    if (isInProgress) {
+                        title = this.labelReference.inProgressTitle.replace("{0}", releaseGateLabel);
+                        message = this.labelReference.releaseGateActivateInProgress.replace("{0}", releaseGateLabel);
                         toastType = "info";
-                        toastMode = "dismissable";
+                        toastMode = "sticky";
                     } else {
                         message = this.labelReference.releaseGateActivateSuccess.replace("{0}", releaseGateLabel);
-                        title = this.labelReference.success;
+                        title = this.labelReference.successTitle;
                         toastType = "success";
                         toastMode = "sticky";
                     }
